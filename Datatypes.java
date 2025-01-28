857ba6e21d2f6c104ea57e5118b0b12b51824273
PushpaSri1306 <puttapushpasri@gmail.com>
2025-01-28 20:12:22 +0530

    Datatypes

---
+++ Datatypes.java
@@ -0,0 +1,18 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        byte a = 25;
+        System.out.println(a);
+        short b = 300;
+        System.out.println(b);
+        long c = 1001000;
+        System.out.println(c);
+        int d = 2;
+        System.out.println(d);
+        float e = 20;
+        System.out.println(e);
+        double f = 20d;
+        System.out.println(f);
+        boolean g = true;
+        System.out.println(g);
+    }
+}
